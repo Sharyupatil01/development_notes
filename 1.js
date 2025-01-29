7e89ee2410a45014d701800fe8321d2154357c62
Sharyu Patil <139983419+Sharyupatil01@users.noreply.github.com>
2025-01-29 22:25:09 +0530

    Basis of javascript datatype's, operators  etc

---
+++ 1.js
@@ -0,0 +1,39 @@
+// let a=4+"5"+6;
+// console.log(a);
+// const cars=["Audi","sujuki","bwm"];
+// cars[0]="benz mercedes";
+// console.log(cars);
+// cars.push("f1");
+// console.log(cars);
+
+// const house={type:"kitchen",model:"1500",color:"white"};
+// console.log(house);
+// house.type="bedroom";
+// console.log(house);
+// const int1=10;
+// console.log(int1);
+// {
+//     const int1=20;
+//     console.log(int1);
+// }
+// let a="sharyu";
+// let b="siaryu";
+// let result=a<b;
+// console.log(result);
+// let c="30";
+// let d="3";
+// let res=c>d;
+// console.log(res);
+// console.log(typeof(a));
+let x=16+"Volvo";
+console.log(x);
+let y="volvo"+16;
+console.log(y);
+let z="volvo"+14+12;
+console.log(z);
+let s=14+12+"volvo";
+console.log(s);
+
+
+
+      
