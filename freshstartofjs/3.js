bf15f8e2d673702ab55193f82037f20d21344d8b
Sharyu Patil <139983419+Sharyupatil01@users.noreply.github.com>
2025-01-31 09:10:02 +0530

    Loops and Challengenes

---
+++ freshstartofjs/3.js
@@ -0,0 +1,48 @@
+// Loops in javascript
+// conditions in javascript 
+// Checking if a number is greater than another number:
+let num1=9;
+let num2=10;
+if(num1>num2)
+{
+    console.log("num1 is greater ");
+}
+else{
+    console.log("num2 is greater");
+}
+
+// checking if a string is equal to another  string 
+
+let username="chai"
+let  anotherusername="chaiwithcode"
+if(username==anotherusername)
+{
+    console.log("two strings are equal");
+}
+else{
+    console.log("They are not equal ");
+}
+
+
+// check is the variable is number or not 
+
+let a=11;
+if(typeof a === "number")
+{
+    console.log("yes it is number");
+}
+else{
+    console.log("it is not a number");
+}
+
+
+// check if the array is empty or not 
+
+let arr=[];
+if(arr.length==0)
+{
+    console.log("it is empty");
+}
+else{
+    console.log("its not empty");
+}
